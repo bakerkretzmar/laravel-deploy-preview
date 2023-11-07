2b682600fd9fd8d9dbf57e83f66d3bbb906548d5
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2023-11-07 13:25:10 -0500

    Handle `environment` with `DB_CONNECTION=sqlite`

--- src/main.ts
+++ src/main.ts
@@ -106,6 +106,7 @@ export async function run() {
       await destroyPreview({
         branch: pr.pull_request.head.ref,
         servers,
+        environment,
       });
     }
   } catch (error) {
